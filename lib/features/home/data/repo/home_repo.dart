de9ae35a54f3869a9a7b84a1b9abb6c61b419ee6
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-09-04 14:10:15 +0300

    getCategories home repo

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:homewood/core/error/failure.dart';
 import 'package:homewood/features/home/data/model/banner_model.dart';
+import 'package:homewood/features/home/data/model/categories_model.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, BannerModel>> getBanner();
+  Future<Either<Failure, CategoriesModel>> getCategories();
 }
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:homewood/core/error/failure.dart';
 import 'package:homewood/core/service/app_service.dart';
 import 'package:homewood/features/home/data/model/banner_model.dart';
+import 'package:homewood/features/home/data/model/categories_model.dart';
 import 'package:homewood/features/home/data/repo/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
@@ -15,6 +16,19 @@ class HomeRepoImpl implements HomeRepo {
     try {
       BannerModel bannerModel = await apiService.getBanners();
       return right(bannerModel); 
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e.type));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+  
+  @override
+  Future<Either<Failure, CategoriesModel>> getCategories() async{
+    try {
+      CategoriesModel categoriesModel = await apiService.getCategories();
+      return right(categoriesModel); 
     } catch (e) {
       if (e is DioException) {
         return left(ServerFailure.fromDioError(e.type));
