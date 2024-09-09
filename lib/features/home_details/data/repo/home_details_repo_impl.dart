07d100ed1c67d75335264d05e514024a7a0d04e1
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-09-09 20:10:59 +0300

    get product details service and repo

---
+++ lib/features/home_details/data/repo/home_details_repo_impl.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:homewood/core/error/failure.dart';
+import 'package:homewood/core/service/app_service.dart';
+import 'package:homewood/features/home_details/data/model/product_details_model.dart';
+import 'package:homewood/features/home_details/data/repo/home_details_repo.dart';
+
+class HomeDetailsRepoImpl implements HomeDetailsRepo {
+  final ApiService apiService;
+  HomeDetailsRepoImpl({required this.apiService});
+  @override
+  Future<Either<Failure, ProductDetailsModel>> getProductDetails({required productId}) async{
+    try {
+      ProductDetailsModel productDetailsModel = await apiService.getProductDetails(productId: productId);
+      return right(productDetailsModel); 
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e.type));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+    }
+}
